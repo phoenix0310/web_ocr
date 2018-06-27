02b34dc412addcf0322300a6dc712a2f0039bd73
phoenix0310 <phoenix03101990@gmail.com>
2018-06-27 22:19:24 +0700

    update demo

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (function () {
 'use strict';
 
-angular.module('WebOCRApp',['hxPhotos','angular-screenshot','pdfjsViewer'])
+angular.module('WebOCRApp',['hxPhotos','angular-screenshot','pdf'])
        .controller('AdminController', AdminController);
 
 
@@ -80,7 +80,36 @@ angular.module('WebOCRApp',['hxPhotos','angular-screenshot','pdfjsViewer'])
 
     
 
+    $scope.pdfName = 'Relativity: The Special and General Theory by Albert Einstein';
+    $scope.pdfUrl = 'upload/1.pdf';
+    $scope.pdfPassword = 'test';
+    $scope.scroll = 0;
+    $scope.loading = 'loading';
   
+    $scope.getNavStyle = function(scroll) {
+      if(scroll > 100) return 'pdf-controls fixed';
+      else return 'pdf-controls';
+    }
+  
+    $scope.onError = function(error) {
+      console.log(error);
+    }
+  
+    $scope.onLoad = function() {
+      $scope.loading = '';
+    }
+  
+    $scope.onProgress = function (progressData) {
+      console.log(progressData);
+    };
+  
+    $scope.onPassword = function (updatePasswordFn, passwordResponse) {
+      if (passwordResponse === PDFJS.PasswordResponses.NEED_PASSWORD) {
+          updatePasswordFn($scope.pdfPassword);
+      } else if (passwordResponse === PDFJS.PasswordResponses.INCORRECT_PASSWORD) {
+          console.log('Incorrect password')
+      }
+    };
   
   
   
