324b32a4d0767d1a7ea97889345993e276d4ea18
phoenix0310 <phoenix03101990@gmail.com>
2018-06-27 07:44:12 +0700

    huhu

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 (function () {
 'use strict';
 
-angular.module('WebOCRApp',['hxPhotos'])
+angular.module('WebOCRApp',['hxPhotos','angular-screenshot'])
        .controller('AdminController', AdminController);
 
 
-  AdminController.$inject = ['WebcamService'];
-  function AdminController(WebcamService){
+  AdminController.$inject = ['WebcamService','$scope'];
+  function AdminController(WebcamService,$scope){
     var $ctrl=this;
 
-    $ctrl.imgData=new Image();
-    $ctrl.imgData.src='/upload/test.PNG'
+    // $ctrl.imgData=new Image();
+    // $ctrl.imgData.src='/upload/test.PNG'
 
     // $ctrl.data=4;
     // $ctrl.webcam = WebcamService.webcam;
@@ -33,5 +33,57 @@ angular.module('WebOCRApp',['hxPhotos'])
            div.innerHTML += result.html;
          });
     };
+  
+    
+    $ctrl.advanceApi;
+    $ctrl.cancel = cancel;
+    $ctrl.download = download;
+    $ctrl.downloadFull = downloadFull;
+    $ctrl.imageApi;
+    $ctrl.isOpenScreenshot = false;
+    $ctrl.openScreenshot = openScreenshot;
+    $ctrl.sendImage = sendImage;
+    $ctrl.target1Options = {
+       filename: 'target1.png',
+       downloadText: 'Download me',
+       cancelText: 'Close it!'
+    };
+
+    function cancel() {
+       if ($ctrl.advanceApi) $ctrl.advanceApi.cancel();
+    }
+
+
+    function download() {
+       if ($ctrl.advanceApi) $ctrl.advanceApi.download();
+    }
+
+    function downloadFull() {
+       if ($ctrl.fullScreenApi) $ctrl.fullScreenApi.downloadFull();
+    }
+
+    function openScreenshot() {
+       $ctrl.isOpenScreenshot = !$ctrl.isOpenScreenshot;
+    }
+
+    function sendImage() {
+
+      console.log('sasas');
+       if ($ctrl.imageApi) {
+          $ctrl.imageApi.toPng(function (dataUrl) {
+           
+            $ctrl.imgData=dataUrl
+          });
+       }
+    }
+ 
+
+    
+
+  
+  
+  
+  
+  
   }
 })();
